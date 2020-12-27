35272b6b38f10c3dbaf4bfb395fe6709ef14dd6f
Zzzzz1357 <vyacheslav17@mail.ru>
2020-12-27 20:26:44 +0300

    complete slideshow

--- src/app/pages/performers/components/card/card.component.ts
+++ src/app/pages/performers/components/card/card.component.ts
@@ -8,9 +8,36 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
     isMobile = false;
     public isFavorite = false;
+    public bigX = 0;
+    public smallX = 0;
     @Input() card: any
 
     constructor() { }
+
+    next() {
+      if (this.bigX === -(this.card.gallery.length - 1) * 240) {
+        this.smallX = 0;
+        this.bigX = 0;
+      } else {
+        this.bigX -= 240;
+        this.smallX -= 83
+      }
+    }
+
+    prev() {
+      if (this.bigX === 0) {
+        this.bigX = -(this.card.gallery.length - 1) * 240;
+        this.smallX = -(this.card.gallery.length - 1) * 83;
+      } else {
+        this.bigX += 240;
+        this.smallX += 83;
+      }
+    }
+
+    changeSlide(index) {      
+      this.bigX = -index * 240;
+      this.smallX = -index * 83;
+    }
 
     ngOnInit(): void {
         if (window.innerWidth <= 767) { this.isMobile = true }
