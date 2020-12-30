7a76e918c811517fe99214801e90b529a0d40c78
Zzzzz1357 <vyacheslav17@mail.ru>
2020-12-30 19:23:34 +0300

    add fullscreen leaflet

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -12,6 +12,11 @@ export class MapComponent implements OnInit {
   @Input() markers: Marker[];
   public toggle = false;
   map: Map;
+  public fullscreenOptions: {[key:string]:any} = {
+    position: 'topleft',
+    title: 'View Fullscreen',
+    titleCancel: 'Exit Fullscreen',
+  };
 
   constructor(public mapSrv: MapService) {
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { NgxLeafletFullscreenModule } from '@runette/ngx-leaflet-fullscreen';
 import { MapComponent } from '../components/map/map.component';
 import { BreadcrumbsComponent } from '../components/breadcrumbs/breadcrumbs.component';
 import { TagsComponent } from '../components/tags/tags.component';
@@ -22,11 +23,14 @@ import { PaginationComponent } from '../components/pagination/pagination.compone
         CommonModule,
         MaterialModule,
         LeafletModule,
+        NgxLeafletFullscreenModule,
         RouterModule
     ],
   exports: [
         MaterialModule,
         MapComponent,
+        LeafletModule,
+        NgxLeafletFullscreenModule,
         BreadcrumbsComponent,
         TagsComponent,
         RatingStarsComponent,
