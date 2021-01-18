a11a88a26d4b9f35c236900ac33aa3679a218cfd
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-01-18 19:59:56 +0500

    add Reviews mobile

--- src/app/pages/performer/components/reviews/reviews.component.ts
+++ src/app/pages/performer/components/reviews/reviews.component.ts
@@ -1,5 +1,18 @@
 import { Component } from "@angular/core";
 
+export interface IReviews {
+    avatar: string,
+    name: string,
+    date: string,
+    rating: number,
+    titleComment: string,
+    comment: string,
+    amountOfComments: number,
+    likesOrDislikes: number,
+    replies: any,
+    showReplies: boolean
+}
+
 @Component({
     selector: 'app-reviews',
     templateUrl: './reviews.component.html',
@@ -8,14 +21,12 @@ import { Component } from "@angular/core";
 
 export class reviewsComponent{
 
-    showReplies: boolean = false
-
     reviews: any = [
         {
             avatar: '../../../../../assets/images/performer/avatar.png',
             name: 'Иван Алексеев',
             date: '22 января 2020 в 12:44',
-            rating: 5.0,
+            rating: 5,
             titleComment: 'Мне понравилось!',
             comment: 'В рамках кворка, напишу 6000 символов для вашего сайта. Если вашему сайту нужны качественные и интересные тексты, которые смогут заставить посетителей оставаться на нём как можно дальше, то вы попали по адресу. За кворк можно заказать хоть 10 статей, главное чтоб общий объём был 6000 символов без пробелов.',
             amountOfComments: 13,
@@ -33,13 +44,14 @@ export class reviewsComponent{
                     date: '24 января 2020 в 12:44',
                     comment: 'В рамках кворка, напишу 6000 символов для вашего сайта. Если вашему сайту нужны качественные и интересные тексты.'
                 }
-            ]
+            ],
+            showReplies: false // Говнокод!
         },
         {
             avatar: '../../../../../assets/images/performer/avatar.png',
             name: 'Иван Алексеев',
             date: '22 января 2020 в 12:44',
-            rating: 2.0,
+            rating: 2,
             titleComment: 'Очень не очень!',
             comment: 'В рамках кворка, напишу 6000 символов для вашего сайта. Если вашему сайту нужны качественные и интересные тексты, которые смогут заставить посетителей оставаться на нём как можно дальше, то вы попали по адресу. За кворк можно заказать хоть 10 статей, главное чтоб общий объём был 6000 символов без пробелов.',
             likesOrDislikes: 23,
@@ -56,13 +68,14 @@ export class reviewsComponent{
                     date: '24 января 2020 в 12:44',
                     comment: 'В рамках кворка, напишу 6000 символов для вашего сайта. Если вашему сайту нужны качественные и интересные тексты.'
                 }
-            ]
+            ],
+            showReplies: false // Говнокод!
         },
         {
             avatar: '../../../../../assets/images/performer/avatar.png',
             name: 'Иван Алексеев',
             date: '22 января 2020 в 12:44',
-            rating: 3.0,
+            rating: 3,
             titleComment: 'Так себе, но пойдет!',
             comment: 'В рамках кворка, напишу 6000 символов для вашего сайта. Если вашему сайту нужны качественные и интересные тексты, которые смогут заставить посетителей оставаться на нём как можно дальше, то вы попали по адресу. За кворк можно заказать хоть 10 статей, главное чтоб общий объём был 6000 символов без пробелов.',
             amountOfComments: 13,
@@ -80,10 +93,12 @@ export class reviewsComponent{
                     date: '24 января 2020 в 12:44',
                     comment: 'В рамках кворка, напишу 6000 символов для вашего сайта. Если вашему сайту нужны качественные и интересные тексты.'
                 }
-            ]
+            ],
+            showReplies: false // Говнокод!
         }
     ]
 
+    wees = [5,4,3,2,1]
 
 
     getReviewBorderColor(rating: number): string {
@@ -104,6 +119,24 @@ export class reviewsComponent{
         } else if (rating < 3) {
             return 'linear-gradient(0deg, rgba(234, 69, 69, 0.05), rgba(234, 69, 69, 0.05)), #FFFFFF'
         }
+    }
+
+
+
+    showPercentInLine(rating: number): number {
+        let total = this.reviews.length;
+        let amount = this.getAmountOfComments(rating);
+        return this.getPercentOf(total, amount)
+    }
+
+    getAmountOfComments(rating: number): number {
+        return this.reviews.filter((review: IReviews) => {
+            return review.rating === rating
+        }).length;
+    }
+
+    getPercentOf(total: number, amount: number): number {
+        return amount / total * 100
     }
 
 }
