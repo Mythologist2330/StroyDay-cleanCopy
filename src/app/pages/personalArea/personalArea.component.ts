a42e5d6301627ba007a1551bc37c7430406d5fce
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-01-31 03:50:53 +0500

    add PersonalArea page

---
+++ src/app/pages/personalArea/personalArea.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-personalArea',
+    templateUrl: './personalArea.component.html',
+    styleUrls: ['./personalArea.component.scss']
+})
+
+export class PersonalAreaComponent{
+
+
+
+}
