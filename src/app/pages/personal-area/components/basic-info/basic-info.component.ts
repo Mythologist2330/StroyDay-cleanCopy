40506b24a4369fd507e6fdc1b24dde32995290db
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-02-05 02:07:34 +0500

    add data for BasicInfo

--- src/app/pages/personal-area/components/basic-info/basic-info.component.ts
+++ src/app/pages/personal-area/components/basic-info/basic-info.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Performer } from 'src/app/models/Performer';
 
 @Component({
     selector: 'app-basicInfo',
@@ -8,6 +9,6 @@ import { Component } from '@angular/core';
 
 export class BasicInfoComponent{
 
-
+    @Input() performer: Performer
 
 }
