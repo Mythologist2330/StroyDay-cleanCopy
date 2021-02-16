33f224233b9b2fee54544a461903c604a00a69ce
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-02-16 18:11:18 +0500

    fix slider

--- src/app/pages/performer/components/slider/slider.component.ts
+++ src/app/pages/performer/components/slider/slider.component.ts
@@ -1,5 +1,5 @@
 import { animate, state, style, transition, trigger } from "@angular/animations";
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-slider',
@@ -8,47 +8,92 @@ import { Component, Input } from "@angular/core";
     animations: [
         trigger('slider', [
             state('start', style({
-                transform: `translateX(-410px)`,
+                transform: `translateX(-560px)`,
             })),
             state('next', style({
-                transform: `translateX(-1226px)`,
+                transform: `translateX(-1352px)`,
             })),
             state('prev', style({
-                transform: `translateX(-1226px)`,
+                transform: `translateX(-1352px)`,
             })),
+
+            state('startTablet', style({
+                transform: `translateX(-450px)`,
+            })),
+            state('nextTablet', style({
+                transform: `translateX(-986px)`,
+            })),
+            state('prevTablet', style({
+                transform: `translateX(-986px)`,
+            })),
+
             transition('start => next', [
                 animate('.2s')
             ]),
             transition('prev => start', [
+                animate('.2s')
+            ]),
+            transition('startTablet => nextTablet', [
+                animate('.2s')
+            ]),
+            transition('prevTablet => startTablet', [
                 animate('.2s')
             ])
         ])
     ]
 })
 
-export class SliderComponent{
+export class SliderComponent implements OnInit{
 
-    public sliderState = 'start';
+    public sliderState
     @Input() gallery: string[];
 
-    next() {
-        this.sliderState = 'next';
-        setTimeout(() => {
-          let currentSlide = this.gallery[0];
-          this.gallery.shift();
-          this.gallery.push(currentSlide);
-          this.sliderState = 'start';
-        }, 200)
+    next(event) {
+        if (event.view.innerWidth > 1275) {
+            this.sliderState = 'next';
+            setTimeout(() => {
+              let currentSlide = this.gallery[0];
+              this.gallery.shift();
+              this.gallery.push(currentSlide);
+              this.sliderState = 'start';
+            }, 200)
+        } else {
+            this.sliderState = 'nextTablet';
+            setTimeout(() => {
+              let currentSlide = this.gallery[0];
+              this.gallery.shift();
+              this.gallery.push(currentSlide);
+              this.sliderState = 'startTablet';
+            }, 200)
+        }
     }
 
-    prev() {
+    prev(event) {
         let currentSlide = this.gallery[this.gallery.length - 1];
         this.gallery.pop();
         this.gallery.unshift(currentSlide);
-        this.sliderState = 'prev';
-        setTimeout(() => {
-          this.sliderState = 'start';
-        }, 0)
+
+        if (event.view.innerWidth > 1275) {
+            this.sliderState = 'prev';
+            setTimeout(() => {
+              this.sliderState = 'start';
+            }, 0)
+        } else {
+            this.sliderState = 'prevTablet';
+            setTimeout(() => {
+              this.sliderState = 'startTablet';
+            }, 0)
+        }
+    }
+
+    ngOnInit() {
+        console.log(window.innerWidth)
+        if (window.innerWidth > 1275) {
+            this.sliderState = 'start'; console.log(window.innerWidth)
+        } else {
+            this.sliderState = 'startTablet'; console.log(window.innerWidth)
+        }
+        
     }
 
 }
