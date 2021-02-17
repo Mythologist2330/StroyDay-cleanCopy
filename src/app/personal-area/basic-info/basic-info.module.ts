f0facfd859cfc83565894c9c3f50f9997f08557c
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-02-18 03:09:58 +0500

    add DepartureAreasComponent

--- src/app/personal-area/basic-info/basic-info.module.ts
+++ src/app/personal-area/basic-info/basic-info.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { SharedModule } from "src/app/shared/shared.module";
 import { BasicInfoComponent } from "./basic-info.component";
+import { DepartureAreasComponent } from "./component/departure-areas/departure-areas.component";
 import { ServiceCategoriesComponent } from "./component/service-categories/service-categories.component";
 
 @NgModule({
   declarations: [
     BasicInfoComponent,
-    ServiceCategoriesComponent
+    ServiceCategoriesComponent,
+    DepartureAreasComponent
   ],
   imports: [
     CommonModule,
