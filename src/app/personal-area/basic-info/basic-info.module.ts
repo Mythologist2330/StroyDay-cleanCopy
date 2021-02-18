b95adaaec4a42524f542717c6492010565e14d93
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-02-19 01:49:16 +0500

    add LocationComponent

--- src/app/personal-area/basic-info/basic-info.module.ts
+++ src/app/personal-area/basic-info/basic-info.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from "src/app/shared/shared.module";
 import { BasicInfoComponent } from "./basic-info.component";
 import { ContactFaceComponent } from "./component/contact-face/contact-face.component";
 import { DepartureAreasComponent } from "./component/departure-areas/departure-areas.component";
+import { LocationComponent } from "./component/location/location.component";
 import { MetroComponent } from "./component/metro/metro.component";
 import { PerformerTypeComponent } from "./component/performer-type/performer-type.component";
 import { ServiceCategoriesComponent } from "./component/service-categories/service-categories.component";
@@ -15,7 +16,7 @@ import { ServiceCategoriesComponent } from "./component/service-categories/servi
     DepartureAreasComponent,
     MetroComponent,
     ContactFaceComponent,
-
+    LocationComponent,
     PerformerTypeComponent
   ],
   imports: [
