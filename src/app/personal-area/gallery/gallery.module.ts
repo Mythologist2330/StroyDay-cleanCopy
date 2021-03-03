81e2add92fa8569b7a911c55ca1e92c98355ddd7
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-03-03 21:43:43 +0500

    replace GalleryAlbumComponent in parent

--- src/app/personal-area/gallery/gallery.module.ts
+++ src/app/personal-area/gallery/gallery.module.ts
@@ -1,15 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { SharedModule } from "src/app/shared/shared.module";
-import { GalleryAlbumComponent } from "./components/gallery-album/gallery-album.component";
 import { NotificationComponent } from "./components/notification/notification.component";
 import { GalleryComponent } from "./gallery.component";
 
 @NgModule({
   declarations: [
     GalleryComponent,
-    NotificationComponent,
-    GalleryAlbumComponent
+    NotificationComponent
   ],
   imports: [
     CommonModule,
