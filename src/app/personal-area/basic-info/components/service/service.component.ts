8d91ddc7b93aaa9f234430389a951f6d169fefdb
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-03-16 04:10:30 +0500

    delete plus from services-categories of BasicInfo

--- src/app/personal-area/basic-info/components/service/service.component.ts
+++ src/app/personal-area/basic-info/components/service/service.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Service } from 'src/app/models/Service';
 
 @Component({
@@ -6,34 +6,28 @@ import { Service } from 'src/app/models/Service';
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.scss']
 })
-export class ServiceComponent implements OnInit {
+export class ServiceComponent {
 
-  @Input() service: Service;
-  public showService = false;
+    @Input() service: Service;
 
-  constructor() {}
-
-  ngOnInit(): void {
-  }
-
-  getColor(segment) {
-    if (segment === 'эконом') {
-        return '#F8601F'
-    } else if (segment === 'стандарт') {
-        return '#0D6FE3'
-    } else if (segment === 'премиум') {
-        return '#9F8C66'
+    getColor(segment) {
+        if (segment === 'эконом') {
+            return '#F8601F'
+        } else if (segment === 'стандарт') {
+            return '#0D6FE3'
+        } else if (segment === 'премиум') {
+            return '#9F8C66'
+        }
     }
-}
 
-getBackground(segment) {
-    if (segment === 'эконом') {
-        return 'linear-gradient(0deg, rgba(248, 96, 31, 0.2), rgba(248, 96, 31, 0.2)), #FFFFFF'
-    } else if (segment === 'стандарт') {
-        return 'linear-gradient(0deg, rgba(13, 111, 227, 0.2), rgba(13, 111, 227, 0.2)), #FFFFFF'
-    } else if (segment === 'премиум') {
-        return 'linear-gradient(0deg, rgba(159, 140, 102, 0.2), rgba(159, 140, 102, 0.2)), #FFFFFF'
+    getBackground(segment) {
+        if (segment === 'эконом') {
+            return 'linear-gradient(0deg, rgba(248, 96, 31, 0.2), rgba(248, 96, 31, 0.2)), #FFFFFF'
+        } else if (segment === 'стандарт') {
+            return 'linear-gradient(0deg, rgba(13, 111, 227, 0.2), rgba(13, 111, 227, 0.2)), #FFFFFF'
+        } else if (segment === 'премиум') {
+            return 'linear-gradient(0deg, rgba(159, 140, 102, 0.2), rgba(159, 140, 102, 0.2)), #FFFFFF'
+        }
     }
-}
 
 }
