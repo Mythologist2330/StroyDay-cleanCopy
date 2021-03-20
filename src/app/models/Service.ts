dafe132b4d3e776532151d21d701111a9109da3f
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-03-20 18:46:26 +0500

    add functins in ServiceModel for segment

--- src/app/models/Service.ts
+++ src/app/models/Service.ts
@@ -39,4 +39,24 @@ export class Service {
     getServiceBySegment(segment: Segment) {
         return this.subServices.filter(sub => sub.segment === segment)
     }
+
+    getColor(segment) {
+        if (segment === 'эконом') {
+            return '#F8601F'
+        } else if (segment === 'стандарт') {
+            return '#0D6FE3'
+        } else if (segment === 'премиум') {
+            return '#9F8C66'
+        }
+    }
+
+    getBackground(segment) {
+        if (segment === 'эконом') {
+            return 'linear-gradient(0deg, rgba(248, 96, 31, 0.2), rgba(248, 96, 31, 0.2)), #FFFFFF'
+        } else if (segment === 'стандарт') {
+            return 'linear-gradient(0deg, rgba(13, 111, 227, 0.2), rgba(13, 111, 227, 0.2)), #FFFFFF'
+        } else if (segment === 'премиум') {
+            return 'linear-gradient(0deg, rgba(159, 140, 102, 0.2), rgba(159, 140, 102, 0.2)), #FFFFFF'
+        }
+    }
 }
