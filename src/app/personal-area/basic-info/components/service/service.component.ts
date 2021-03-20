cd973ee112072f2a738a10508f765aaa048d850d
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-03-20 19:55:51 +0500

    fix styles for segments

--- src/app/personal-area/basic-info/components/service/service.component.ts
+++ src/app/personal-area/basic-info/components/service/service.component.ts
@@ -7,7 +7,7 @@ import { Service } from 'src/app/models/Service';
   styleUrls: ['./service.component.scss']
 })
 
-export class ServiceComponent implements OnInit{
+export class ServiceComponent implements OnInit {
 
     @Input() service: Service;
     isMobile: boolean;
@@ -17,26 +17,6 @@ export class ServiceComponent implements OnInit{
 			this.isMobile = true
 		} else {
             this.isMobile = false
-        }
-    }
-
-    getColor(segment) {
-        if (segment === 'эконом') {
-            return '#F8601F'
-        } else if (segment === 'стандарт') {
-            return '#0D6FE3'
-        } else if (segment === 'премиум') {
-            return '#9F8C66'
-        }
-    }
-
-    getBackground(segment) {
-        if (segment === 'эконом') {
-            return 'linear-gradient(0deg, rgba(248, 96, 31, 0.2), rgba(248, 96, 31, 0.2)), #FFFFFF'
-        } else if (segment === 'стандарт') {
-            return 'linear-gradient(0deg, rgba(13, 111, 227, 0.2), rgba(13, 111, 227, 0.2)), #FFFFFF'
-        } else if (segment === 'премиум') {
-            return 'linear-gradient(0deg, rgba(159, 140, 102, 0.2), rgba(159, 140, 102, 0.2)), #FFFFFF'
         }
     }
 
