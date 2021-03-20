03b7f1227d820e87f56abdebda46141e6d2bd3fe
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-03-20 23:04:31 +0500

    fix scroll 0 0

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pages',
@@ -6,8 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./pages.component.scss']
 })
 
-export class PagesComponent {
+export class PagesComponent implements OnInit {
 
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0)
+    });
+  }
 
 }
