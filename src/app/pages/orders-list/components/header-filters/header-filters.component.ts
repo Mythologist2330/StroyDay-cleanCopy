d85b520ed907566cf9a968e4c389fabd4e7715b3
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-04-04 14:55:10 +0500

    add complete and fixed overlay

--- src/app/pages/orders-list/components/header-filters/header-filters.component.ts
+++ src/app/pages/orders-list/components/header-filters/header-filters.component.ts
@@ -23,20 +23,17 @@ export class HeaderFiltersComponent {
     }
 
     onOffScroll(event) {
-        let html = event.path.filter(pathElem => pathElem.localName === 'html')
-        let body = event.path.filter(pathElem => pathElem.localName === 'body')
+        event.path.filter((elem) => {
+			if (elem.localName === ('html' || 'body')) {
 
-        if (html[0].style.overflow === 'hidden') {
-            html[0].style.overflow = 'auto'
-        } else {
-            html[0].style.overflow = 'hidden'
-        }
+				if (elem.style.overflow === 'hidden') {
+					elem.style.overflow = 'auto';
+				} else {
+					elem.style.overflow = 'hidden';
+				}
 
-        if (body[0].style.overflow === 'hidden') {
-            body[0].style.overflow = 'auto'
-        } else {
-            body[0].style.overflow = 'hidden'
-        }
+			}
+        })
     }
 
 }
