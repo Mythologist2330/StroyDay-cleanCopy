99a13e22019140b12e130c716bc9f47c126348d3
Mythologist2330 <66572124+Mythologist2330@users.noreply.github.com>
2021-04-04 14:49:19 +0500

    fix header

--- src/app/pages/components/header/header.component.ts
+++ src/app/pages/components/header/header.component.ts
@@ -8,112 +8,120 @@ import { ServicesService } from 'src/app/services/services.service';
 import { Service } from 'src/app/models/Service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 
 export class HeaderComponent implements OnInit, AfterViewInit {
 
-  burgerMenuToggle = false;
-  navigationModuleWindowToggle = false;
-  public profileToogle = false;
-  public menuToggle = false;
-  public currentCategory: Category;
-  public searchText = '';
-  @ViewChild('input') input: ElementRef;
+    burgerMenuToggle: Boolean;
+    navigationModuleWindowToggle = false;
+    public profileToogle = false;
+    public menuToggle = false;
+    public currentCategory: Category;
+    public searchText = '';
+    @ViewChild('input') input: ElementRef;
 
-  constructor(
-    public catSrv: CategoryService,
-    private srvSrv: ServicesService,
-    private router: Router
-  ) {}
+    constructor(
+        public catSrv: CategoryService,
+        private srvSrv: ServicesService,
+        private router: Router
+    ) {}
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    fromEvent(this.input.nativeElement,'keyup')
-      .pipe(
-          filter(Boolean),
-          debounceTime(400),
-          distinctUntilChanged(),
-          tap(() => this.searchText = this.input.nativeElement.value)
-      )
-      .subscribe();
+    ngOnInit(): void {
+        if (window.innerWidth < 1276) {
+            if (window.innerWidth < 768) {
+                this.burgerMenuToggle = false
+            } else {
+                this.burgerMenuToggle = true
+            }
+        }
     }
 
-  getResult(): Observable<any[]> {
-    return combineLatest([this.catSrv.categories$, this.srvSrv.services$], (categories, services) => {
-      return [
-        ...categories.filter(cat => cat.title.toLowerCase().includes(this.searchText.toLowerCase())),
-        ...services.filter(srv => srv.title.toLowerCase().includes(this.searchText.toLowerCase()))
-      ] 
-    });
-  }
-
-  chooseCategory(cat: Category) {
-    this.currentCategory = cat;
-  }
-
-  getEven(subcat: Category[]) {
-    return subcat.filter(item => subcat.indexOf(item) % 2 === 0)
-  }
-
-  getOdd(subcat: Category[]) {  
-    return subcat.filter(item => subcat.indexOf(item) % 2 !== 0)
-  }
-
-  goToCategory(point: Category | Service) {
-    if (point instanceof Category) {
-      this.router.navigate(['pages/services/service-catalog/' + point.id]);
-    } else {      
-      this.router.navigate(['pages/service/' + point.id]);
+    ngAfterViewInit(): void {
+        fromEvent(this.input.nativeElement,'keyup')
+        .pipe(
+            filter(Boolean),
+            debounceTime(400),
+            distinctUntilChanged(),
+            tap(() => this.searchText = this.input.nativeElement.value)
+        )
+        .subscribe();
     }
-    this.menuToggle = false;
-    this.searchText = null;
-    this.input.nativeElement.value = null;
-  }
 
-  openCloseBurgerMenu(event) {
-    if (event.view.innerWidth <= 767) {
-      this.burgerMenuToggle = !this.burgerMenuToggle
-      this.onOffScroll(event);
+    getResult(): Observable<any[]> {
+        return combineLatest([this.catSrv.categories$, this.srvSrv.services$], (categories, services) => {
+        return [
+            ...categories.filter(cat => cat.title.toLowerCase().includes(this.searchText.toLowerCase())),
+            ...services.filter(srv => srv.title.toLowerCase().includes(this.searchText.toLowerCase()))
+        ] 
+        });
     }
-  }
 
-  navigationModuleToggleMobile(event) {
-    if (event.view.innerWidth <= 767) {
-
-      let modalWindow = document.getElementById('navigationModuleWindow')
-      this.menuToggle = true
-
-      if (modalWindow.style.right === '-16px') {
-        modalWindow.style.right = 'calc(-100% - 48px)'
-
-        setTimeout(() => {
-          this.menuToggle = false
-        }, 200);
-
-      } else {
-        setTimeout(() => {
-          modalWindow.style.right = '-16px'
-        }, 0);
-      }
-
+    chooseCategory(cat: Category) {
+        this.currentCategory = cat;
     }
-  }
 
-  onOffScroll(event) {
-    event.path.filter((elem) => {
-      if (elem.localName === ('html' || 'body')) {
+    getEven(subcat: Category[]) {
+        return subcat.filter(item => subcat.indexOf(item) % 2 === 0)
+    }
 
-        if (elem.style.overflow === 'hidden') {
-          elem.style.overflow = 'auto';
+    getOdd(subcat: Category[]) {  
+        return subcat.filter(item => subcat.indexOf(item) % 2 !== 0)
+    }
+
+    goToCategory(point: Category | Service) {
+        if (point instanceof Category) {
+        this.router.navigate(['pages/services/service-catalog/' + point.id]);
+        } else {      
+        this.router.navigate(['pages/service/' + point.id]);
+        }
+        this.menuToggle = false;
+        this.searchText = null;
+        this.input.nativeElement.value = null;
+    }
+
+    openCloseBurgerMenu(event) {
+        if (event.view.innerWidth <= 767) {
+        this.burgerMenuToggle = !this.burgerMenuToggle
+        this.onOffScroll(event);
+        }
+    }
+
+    navigationModuleToggleMobile(event) {
+        if (event.view.innerWidth <= 767) {
+
+        let modalWindow = document.getElementById('navigationModuleWindow')
+        this.menuToggle = true
+
+        if (modalWindow.style.right === '-16px') {
+            modalWindow.style.right = 'calc(-100% - 48px)'
+
+            setTimeout(() => {
+            this.menuToggle = false
+            }, 200);
+
         } else {
-          elem.style.overflow = 'hidden';
+            setTimeout(() => {
+            modalWindow.style.right = '-16px'
+            }, 0);
         }
 
-      }
-    })
-  }
+        }
+    }
+
+    onOffScroll(event) {
+        event.path.filter((elem) => {
+        if (elem.localName === ('html' || 'body')) {
+
+            if (elem.style.overflow === 'hidden') {
+            elem.style.overflow = 'auto';
+            } else {
+            elem.style.overflow = 'hidden';
+            }
+
+        }
+        })
+    }
 }
